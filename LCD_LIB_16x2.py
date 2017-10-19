d04e3539835e855a8b9a7339057b346934903c0f
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-10-19 10:46:05 +0200

    Creada función para inicializar enviando datos a la LCD

--- LCD_LIB_16x2.py
+++ LCD_LIB_16x2.py
@@ -60,3 +60,45 @@ def lcd_init():
     lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
     lcd_byte(0x01,LCD_CMD) # 000001 Clear display
     time.sleep(E_DELAY)
+
+def lcd_byte(bits, mode):
+    # Send byte to data pins
+    # bits = data
+    # mode = True for character
+    #        False for command
+
+    GPIO.output(LCD_RS, mode) # RS
+
+    # High bits
+    GPIO.output(LCD_D4, False)
+    GPIO.output(LCD_D5, False)
+    GPIO.output(LCD_D6, False)
+    GPIO.output(LCD_D7, False)
+    if bits&0x10==0x10:
+        GPIO.output(LCD_D4, True)
+    if bits&0x20==0x20:
+        GPIO.output(LCD_D5, True)
+    if bits&0x40==0x40:
+        GPIO.output(LCD_D6, True)
+    if bits&0x80==0x80:
+        GPIO.output(LCD_D7, True)
+
+    # Toggle 'Enable' pin
+    lcd_toggle_enable()
+
+    # Low bits
+    GPIO.output(LCD_D4, False)
+    GPIO.output(LCD_D5, False)
+    GPIO.output(LCD_D6, False)
+    GPIO.output(LCD_D7, False)
+    if bits&0x01==0x01:
+        GPIO.output(LCD_D4, True)
+    if bits&0x02==0x02:
+        GPIO.output(LCD_D5, True)
+    if bits&0x04==0x04:
+        GPIO.output(LCD_D6, True)
+    if bits&0x08==0x08:
+        GPIO.output(LCD_D7, True)
+
+   # Toggle 'Enable' pin
+   lcd_toggle_enable()
