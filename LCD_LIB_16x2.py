83cccd34a560081566f14bd52c5cb4b6c5ce5ecd
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-10-19 10:44:33 +0200

    Función para inicializar la pantalla

--- LCD_LIB_16x2.py
+++ LCD_LIB_16x2.py
@@ -47,3 +47,16 @@ def init_GPIO():
     GPIO.setup(LCD_D5, GPIO.OUT) # DB5
     GPIO.setup(LCD_D6, GPIO.OUT) # DB6
     GPIO.setup(LCD_D7, GPIO.OUT) # DB7
+
+# Inicializar pantalla LCD
+def lcd_init():
+    # Activa pines GPIO
+    init_GPIO()
+
+    lcd_byte(0x33,LCD_CMD) # 110011 Initialise
+    lcd_byte(0x32,LCD_CMD) # 110010 Initialise
+    lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
+    lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
+    lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
+    lcd_byte(0x01,LCD_CMD) # 000001 Clear display
+    time.sleep(E_DELAY)
