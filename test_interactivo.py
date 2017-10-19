fc9faf4136ada0ee2e4710c717b62422463f2572
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-10-19 12:42:00 +0200

    Importada librería para control de LCD

--- test_interactivo.py
+++ test_interactivo.py
@@ -15,14 +15,15 @@
 ##    Importar Librerías    ##
 ##############################
 
+import LCD_LIB_16x2 as LCD
+
 ##############################
 ##         Variables        ##
 ##############################
 
-def opciones():
+def mostrar_opciones():
     print('''
         1) Cambiar línea 1 de la pantalla LCD
         2) Cambiar línea 2 de la pantalla LCD
         0) Salir del script
     ''')
-opciones()
