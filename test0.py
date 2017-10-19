d9e8ee98ba8cf7658df3aad55fb419967174bb6f
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-10-19 10:56:44 +0200

    Creado bucle con  3 mensajes en distintos formatos como prueba

--- test0.py
+++ test0.py
@@ -25,3 +25,23 @@ import LCD_LIB_16x2 as LCD
 
 # Inicializar Pantalla
 LCD.lcd_init()
+
+while True:
+    # Enviando mensaje a cada línea
+    LCD.lcd_string("  Rasbperry Pi ",LCD.LINE_1)
+    LCD.lcd_string("16x2 LCD Pruebas",LCD.LINE_2)
+
+    time.sleep(3) # 3 segundos delay
+
+    # Enviando mensaje a cada línea
+    LCD.lcd_string("1234567890123456",LCD.LINE_1)
+    LCD.lcd_string("abcdefghijklmnop",LCD.LINE_2)
+
+    time.sleep(3) # 3 segundos delay
+
+    # Enviando mensaje a cada línea
+    LCD.lcd_string("    Fryntiz ",LCD.LINE_1)
+    LCD.lcd_string(" www.fryntiz.es ",LCD.LINE_2)
+
+    time.sleep(10)
+GPIO.cleanup()
