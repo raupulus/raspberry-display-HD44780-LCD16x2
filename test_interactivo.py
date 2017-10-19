ca924657b8b251744b8b987f2fce7661fbc8e320
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-10-19 12:47:01 +0200

    Creado menú interactivo

--- test_interactivo.py
+++ test_interactivo.py
@@ -15,7 +15,7 @@
 ##    Importar Librerías    ##
 ##############################
 
-import LCD_LIB_16x2 as LCD
+#import LCD_LIB_16x2 as LCD
 
 ##############################
 ##         Variables        ##
@@ -27,3 +27,23 @@ def mostrar_opciones():
         2) Cambiar línea 2 de la pantalla LCD
         0) Salir del script
     ''')
+
+while True:
+    mostrar_opciones()
+    opciones = {}
+    opcion = raw_input('Opción → ')
+
+    if (opcion == 1):
+        print('Introduce el nuevo valor para la línea 1')
+        tmp = raw_input('Cadena → ')
+        #LCD.lcd_string(tmp, LCD.LINE_1)
+    elif (opcion == 2):
+        print('Introduce el nuevo valor para la línea 2')
+        tmp = raw_input('Cadena → ')
+        #LCD.lcd_string(tmp, LCD.LINE_2)
+    elif (opcion == 0):
+        print('Limpiando y Cerrando Script')
+        #GPIO.cleanup()
+        break;
+    else:
+        print('Opción no válida')
