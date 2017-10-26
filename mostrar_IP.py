3f333a0c12404b75846c07053832c03e2a26144c
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-10-26 13:47:51 +0200

    Creada función para obtener dirección IP

--- mostrar_IP.py
+++ mostrar_IP.py
@@ -19,3 +19,23 @@
 ##############################
 ##         Variables        ##
 ##############################
+
+import urllib2  # Librería para obtener web
+
+# Función para obtener IP
+def getIp():
+    # Variable donde guardar la dirección IP
+    ip = ''
+
+    # Añadir web html a variable
+    web = urllib2.urlopen('http://checkip.dyndns.org').read()
+
+    # Recorrer html descargado en busca de números que conforman la ip
+    for line in str(web):
+        # Extraer números y puntos separadores
+        if line in str(range(10)) + '.':
+            ip += line
+
+    print ('La ip es → ' + ip.strip())
+    return ip.strip()
+
